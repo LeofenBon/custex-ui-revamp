d7213b37874e9e86bc81b9d8c193e87deda9390e
LeofenBon <46328889+LeofenBon@users.noreply.github.com>
2024-01-04 21:28:57 +0400

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,8 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import TailwindThemeProvider from './providers/tailwindThemeProvider'
+import { twMerge } from "tailwind-merge";
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <TailwindThemeProvider>{children}</TailwindThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
